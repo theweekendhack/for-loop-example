65849daefb0be42a5128abc125b04f61d7f382ee
Kadeem Best <kadeem.best@theweekendhack.com>
2021-11-20 11:22:19 -0500

    Simple Foor Loop Example

---
+++ forLoop.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+/*
+    Typically use a for loop, when you KNOW how many times a particular 
+    set of statement(s) are going to be executed!!
+*/
+
+
+
+/*
+
+++ increment operator
+--  decrement operator
+
+
+i++ this is the same as saying i=i+1;
+i-- this is the same as saying i=i-1
+*/
+ 
+let i; // counter variable (Is a variable that keeps track of how many times the loop has happed thus far!)
+for(i=5; i>0; i--)  //header of for loop
+{
+    console.log(`Slice of Pizza`);
+
+}
+console.log("\n\n");
+
+for(i=1; i<=5; i++)  
+{
+    console.log(`Baby, you sweet like honey!`); 
+}
+
+
+
+
+
+
+
